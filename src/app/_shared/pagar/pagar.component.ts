367bce8539f60e7f9f2217d671074d62d49a796c
fernandodamaso <fernando.damaso@hotmail.com>
2022-12-19 17:18:43 -0300

    melhoria na tela de pagamento

--- src/app/_shared/pagar/pagar.component.ts
+++ src/app/_shared/pagar/pagar.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ProdutoModel } from 'src/app/_models/produto.model';
 import { SocioModel } from 'src/app/_models/socio.model';
 import { sociosService } from 'src/app/_services/socios.service';
 
@@ -27,35 +28,44 @@ export class PagarComponent implements OnInit {
   }
 
   informacoesSocio: SocioModel;
-  valorTotal: number;
-  valorRecebel: number;
+  valorTotal = 0;
+  valorReceber = 0;
   desconto = 0;
-
+  debito = 0;
+  formaPagamento = 'pix';
   date = new FormControl(new Date());
   dataCompra: Date;
+  produtosAtivos: ProdutoModel[] = [];
 
   ngOnInit(): void {
+    for (let x = 0; x < this.informacoesSocio.produtosEmAberto.length; x++) {
+      const element = this.informacoesSocio.produtosEmAberto[x];
+      this.produtosAtivos.push(element);
+    }
     this.calcularValorTotal();
   }
 
   calcularValorTotal() {
     let listaPrecoVenda = [];
 
-    for (let i = 0; i < this.informacoesSocio.produtosEmAberto.length; i++) {
-      listaPrecoVenda.push(
-        this.informacoesSocio.produtosEmAberto[i].precoVenda
-      );
+    for (let i = 0; i < this.produtosAtivos.length; i++) {
+      listaPrecoVenda.push(this.produtosAtivos[i].precoVenda);
     }
 
-    let somaTotal: number = listaPrecoVenda
-      .map((a) => a)
-      .reduce(function (a, b) {
-        return a + b;
-      });
-
-    somaTotal = somaTotal * -1;
-    this.valorTotal = somaTotal + this.informacoesSocio.saldo;
-    this.valorRecebel = this.valorTotal;
+    if (this.produtosAtivos.length > 0) {
+      let somaTotal: number = listaPrecoVenda
+        .map((a) => a)
+        .reduce(function (a, b) {
+          return a + b;
+        });
+      somaTotal = somaTotal * -1;
+      this.debito = somaTotal;
+      this.valorTotal = somaTotal + this.informacoesSocio.saldo;
+    } else {
+      this.valorTotal = 0;
+      this.debito = 0;
+    }
+    this.valorReceber = this.valorTotal;
   }
 
   addEvent(event: MatDatepickerInputEvent<Date>) {
@@ -63,4 +73,25 @@ export class PagarComponent implements OnInit {
   }
 
   salvar() {}
+
+  toggleProduto(produto: ProdutoModel, i: number) {
+    const index = this.produtosAtivos.findIndex((index) => {
+      return index === produto;
+    });
+
+    if (index > -1) {
+      this.produtosAtivos.splice(index, 1);
+    } else {
+      this.produtosAtivos.push(produto);
+    }
+
+    this.calcularValorTotal();
+  }
+
+  verificaProduto(produto: ProdutoModel) {
+    const frutaIndex = this.produtosAtivos.findIndex((el) => {
+      return el === produto;
+    });
+    return frutaIndex > -1;
+  }
 }
