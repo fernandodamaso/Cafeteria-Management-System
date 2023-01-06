207d1ddad73710f931e887744c73b78071f585cd
fernandodamaso <fernando.damaso@hotmail.com>
2023-01-06 16:42:58 -0300

    removendo acentos no pipe de busca por palavras

--- src/app/_shared/buscaPalavrasPipe/busca-palavras.pipe.ts
+++ src/app/_shared/buscaPalavrasPipe/busca-palavras.pipe.ts
@@ -4,9 +4,29 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "buscaPalavras",
 })
 export class BuscaPalavrasPipe implements PipeTransform {
-  transform(listItems: any[], filterSocios: string) {
-    if (filterSocios) {
-      return listItems.filter((item: any) => item.nome.toLowerCase().includes(filterSocios));
+  transform(listItems: any[], busca: string) {
+    if (busca) {
+
+      function replaceSpecialChars(str :string) {
+
+        str = str.toLowerCase();
+        str = str.replace(/[àáâãäå]/, "a");
+        str = str.replace(/[èéêë]/, "e");
+        str = str.replace(/[ií]/,"i");
+        str = str.replace(/[óõô]/,"o");
+        str = str.replace(/[uúû]/,"u");
+        str = str.replace(/[ç]/, "c");
+
+        return str.replace(/[^a-z]/gi, ""); // remove caracteres que não são letras
+      }
+
+      let buscaFiltrada = replaceSpecialChars(busca)
+      console.log(buscaFiltrada);
+
+      return listItems.filter(function(item){
+        let nomeFiltrado = replaceSpecialChars(item.nome);
+        return nomeFiltrado.toLowerCase().includes(buscaFiltrada)
+      })
     } else {
       return listItems;
     }
