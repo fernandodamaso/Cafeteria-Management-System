e04de3433625d50db1c195b7c3b65000ed1ab8d7
fernandodamaso <fernando.damaso@hotmail.com>
2023-01-31 16:12:19 -0300

    Ajuste no filtro de grau

--- src/app/_shared/buscaGrauPipe/busca-grau.pipe.ts
+++ src/app/_shared/buscaGrauPipe/busca-grau.pipe.ts
@@ -5,9 +5,9 @@ import { SocioModel } from "src/app/_models/socio.model";
   name: "buscaGrau",
 })
 export class BuscaGrauPipe implements PipeTransform {
-  transform(listItems: any[], grau: string) {
+  transform(listItems: SocioModel[], grau: string) {
     if (grau) {
-      return listItems.filter((item: any) => item.grau[0].nome.includes(grau));
+      return listItems.filter((item: SocioModel) => item.grau.includes(grau));
     } else {
       return listItems;
     }
